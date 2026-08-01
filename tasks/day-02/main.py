6ca4287386a2bbd2b67bb015c38e7593b9e2c15c
Adrian <adrian.con.work@gmail.com>
2026-08-01 15:30:04 +0300

    just learning how to better use git by pushing from time to time

---
+++ tasks/day-02/main.py
@@ -0,0 +1,22 @@
+# Subscripting
+print("Hello"[2-1])
+
+# String
+print("123546"+"332156")
+
+
+#integer = whole number
+print (123+321)
+
+
+
+#large integers
+print(123_233_123_123)
+
+# float
+print(1.215541)
+# boolean
+print(True)
+print(False)
+
+
