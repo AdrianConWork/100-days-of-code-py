b734ee485a11d1fc228eb8abd1476d82720821c9
Adrian <adrian.con.work@gmail.com>
2026-08-01 14:41:06 +0300

    Day 1: Guild Name Generator

---
+++ tasks/day-01-print-strings-input-variables/main.py
@@ -0,0 +1,39 @@
+## Task 1 Write multiple prints
+
+# print("1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.")
+# print("2. Knead the dough for 10 minutes.")
+# print("3. Add 3g of Salt.")
+# print("4. Leave to rise for 2 hours.")
+# print("5. Bake at 200 degrees C for 30 minutes.")
+
+
+## Task 2 Fix Code
+
+## Fix the code below 👇
+#
+# print(Notes from Day 1")
+#  print("The print statement is used to output strings")
+# print("Strings are strings of characters"
+# priint("String Concatenation is done with the + sign")
+# print(("New lines can be created with a \ and the letter n")
+
+
+
+# print("Notes from Day 1")
+# print("The print statement is used to output strings")
+# print("Strings are strings of characters")
+# print("String Concatenation is done with the + sign")
+#print("New lines can be created with a \ and the letter n")
+
+
+## Task 3 Concatenating inputs and strings
+
+# print("Hello "+input("What is your name?\n")+"!")
+
+# #Task 4 Wrapping an input into a Variable
+# name: str=input("What is your name?")
+# print(name)
+
+# #Task 5 Measuring the length of an input
+name=input("What is your name?")
+print(len(name))
