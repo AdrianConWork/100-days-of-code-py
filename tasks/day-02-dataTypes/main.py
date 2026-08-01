b322dc9ba8d43910fa021ce00eebd1a468de6504
Adrian <adrian.con.work@gmail.com>
2026-08-01 16:32:07 +0300

    just learning how to better use git by pushing from time to time

--- tasks/day-02-dataTypes/main.py
+++ tasks/day-02-dataTypes/main.py
@@ -25,3 +25,23 @@ print(type("sdasd"))
 print(type(123))
 print(type(123.3))
 print(type(True))
+
+# Type Conversion
+print(int(int("124")+float(1.20)))  #adding an int and a float equals a float but if I convert the result into an integer it cuts the .x part
+
+# Make this line of code run without errors
+
+# print("Number of letters in your name: " + len(input("Enter your name")))
+
+print("Number of letters in your name: " + str(len(input("Enter your name\n"))))
+
+
+# BMI Calculator
+height = 1.65
+weight = 84
+
+# Write your code here.
+# Calculate the bmi using weight and height.
+bmi =weight/height**2
+
+print(bmi)
